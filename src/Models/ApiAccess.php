30daa139bbe1cac2da9053fce59d85b95598ce9f
Hamzah <hamzahnafalah@gmail.com>
2025-04-19 23:26:57 +0700

    update modules

--- src/Models/ApiAccess.php
+++ src/Models/ApiAccess.php
@@ -17,58 +17,10 @@ class ApiAccess extends SupportBaseModel
     'id','app_code','reference_id','reference_type','token','props'
   ];
 
-  /**
-   * Set the password attribute using bcrypt
-   *
-   * @param string $value
-   * @return void
-   */
-  public function setPasswordAttribute($value)
-  {
-    $this->attributes['password'] = Hash::make($value);
-  }
+  public function setPasswordAttribute($value){$this->attributes['password'] = Hash::make($value);}
+  public function scopeFindToken($builder, $token){return $builder->where('token', $token);}
+  public function scopeFindAppCode($builder, $app_code){return $builder->where('app_code', $app_code);}
+  public function scopeFindUsername($builder, $username){return $builder->where('username', $username);}
 
-  //LOCAL SCOPE
-  /**
-   * Find ApiAccess model by token
-   *
-   * @param Builder $builder
-   * @param string $token
-   * @return Builder
-   */
-  public function scopeFindToken($builder, $token)
-  {
-    return $builder->where('token', $token);
-  }
-
-  /**
-   * Find ApiAccess model by app_code
-   *
-   * @param Builder $builder
-   * @param string $app_code
-   * @return Builder
-   */
-  public function scopeFindAppCode($builder, $app_code)
-  {
-    return $builder->where('app_code', $app_code);
-  }
-
-  /**
-   * Find ApiAccess model by username
-   *
-   * @param Builder $builder
-   * @param string $username
-   * @return Builder
-   */
-  public function scopeFindUsername($builder, $username)
-  {
-    return $builder->where('username', $username);
-  }
-
-  //EIGER SECTION
-  public function reference()
-  {
-    return $this->morphTo();
-  }
-  //END EIGER SECTION
+  public function reference(){return $this->morphTo();}
 }
